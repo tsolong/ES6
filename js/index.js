e37f95621a91c9aa54cdf6817ff7ad19c3087cd8
tsolong <tsolong@126.com>
2017-08-03 10:33:49 +0800

    es6 let

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function test() {
+    var flag = true;
+    if(flag){
+        var msg = 'hello';
+        let content = '你好';
+    }
+
+    let content = '外层的你好';
+
+    console.log(msg);
+    console.log(content);
+}
+
+test();
+
+console.log(abc);
