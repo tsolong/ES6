fac05b7cb3b43b4a479c1ea6077955012435b140
tsolong <tsolong@126.com>
2017-08-06 22:10:07 +0800

    es6 arrow function

--- js/index.js
+++ js/index.js
@@ -13,4 +13,45 @@ function test() {
 
 test();
 
-console.log(abc);
+function log(x, y = 'World') {
+    console.log(x, y);
+}
+log('Hello');
+log('Hello', 'China');
+log('Hello', '');
+
+function Point(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+}
+
+var P = new Point();
+console.log(P);
+
+var foo = x => x * x;
+console.log(foo(10));
+
+var fooo = () => 200;
+console.log(fooo());
+
+var fooo = (x, y) => {return x + y};
+console.log(fooo(10, 20));
+
+setTimeout( () => console.log('setTimeout'), 1000);
+
+var temp = 'ni hao';
+var tempObj = {temp};
+console.log(tempObj.temp);
+
+var funA = function (x, y) {
+    return {x, y};
+}
+console.log(funA(10, 20));
+
+var obj1 = {
+    name: 'tsolong',
+    showName(){
+        console.log(this.name);
+    }
+}
+obj1.showName();
