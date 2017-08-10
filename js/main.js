9736237b959e9fc981a7f0a3e56a41a650ccf565
tsolong <tsolong@126.com>
2017-08-10 17:43:20 +0800

    es 6 export and import

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import {firstName, lastName, yyear as year, multiply} from './js/profile';
+
+console.log(firstName, lastName, year);
+console.log(multiply(10, 10));
+
---
+++ js/profile.js
@@ -0,0 +1,9 @@
+var firstName = 'tso';
+var lastName = 'long';
+var year = 1989;
+
+var multiply = function (x, y) {
+    return x * y;
+};
+
+export {firstName, lastName, year as yyear, multiply};
